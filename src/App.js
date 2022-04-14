becd78b6eff6d42368850a117f9572cf268983d3
Sohbet <62770431+sohbetguly@users.noreply.github.com>
2022-04-14 18:35:24 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { fetchWeather } from "./api/fetchWeather";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery("");
+    }
+  };
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={search}
+      />
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)} <sup>&deg;</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
